afb34c42a817424226f6dd40d2c5469dde6305ab
Tur4L <tural.bakhtiyarli.2003@gmail.com>
2025-02-16 15:08:09 -0700

    Fix ARIMA: full testing added

--- src/stat_models/auto_arima.py
+++ src/stat_models/auto_arima.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import pmdarima as pm
+import warnings
 from matplotlib import pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
 #-----------------------------------------------------------------------------------------------------
 """ For single window """
 # db = pd.read_csv('./data/db_window_random_10days.csv')
@@ -91,74 +93,64 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolu
 #-----------------------------------------------------------------------------------------------------
 # ''' For multiple windows '''
 df = pd.read_csv('./data/normal/db_final.csv')
-beta2_windows = df.groupby('PtID')
+grouped_df = df.groupby('PtID')
+MAE_t = 0
+MAPE_t = 0
+MAE_i = 0
+MAPE_i = 0
 
+pt_num = 0
 ''' Looping over beta2 scores '''
-for patient_id, window in beta2_windows:
+for ptid, group_data in grouped_df:
     '''Creating the data'''
     # window['timestamp'] = pd.to_datetime(window['timestamp'])
-    window.set_index('DeviceTm', inplace=True)
-    train_90mins = window.iloc[:-6]
-    test_90mins = window.iloc[-6:]
-    train_30mins = window.iloc[:-2]
-    test_30mins = window.iloc[-2:]
+    cut_off = int(len(group_data['Value']) * 0.8)
+    len_data = len(group_data['Value'])
 
-    '''90 minute model: '''
+    for i in range(0, len_data - cut_off - 6):
+        # train_90mins = group_data.iloc[: cut_off + i]
+        # test_90mins = group_data.iloc[cut_off + i : cut_off + i + 18]
+        train_30mins = group_data.iloc[: cut_off + i]
+        test_30mins = group_data.iloc[cut_off + i: cut_off + i + 6]
 
-    model_90mins = pm.auto_arima(train_90mins['Value'], 
-                      start_p=1, start_q=1,      # initial guesses for p and q
-                      max_p=10, max_q=10,          # maximum p and q values to try
-                      seasonal=True,            # no seasonality for CGM data
-                      stepwise=True,             # uses a stepwise approach to find the best model
-                      suppress_warnings=True,    # suppress warnings
-                      trace=False)                # output the results of the search process
+        '''90 minute model: '''
+        # model_90mins = pm.auto_arima(train_90mins['Value'], 
+        #                 start_p=1, start_q=1,      # initial guesses for p and q
+        #                 max_p=10, max_q=10,          # maximum p and q values to try
+        #                 seasonal=True,            # no seasonality for CGM data
+        #                 stepwise=True,             # uses a stepwise approach to find the best model
+        #                 suppress_warnings=True,    # suppress warnings
+        #                 trace=False)                # output the results of the search process
 
-    actuals_90mins = test_90mins['Value']
-    n_periods = len(actuals_90mins)  # Number of periods to forecast
-    forecast_90mins, conf_int_90mins = model_90mins.predict(n_periods=n_periods, return_conf_int=True)
-    # forecast_index = pd.date_range(start=train['glucose mmol/l'].index[-1] + pd.Timedelta(days=1), periods=n_periods)
+        # actuals_90mins = test_90mins['Value']
+        # n_periods = len(actuals_90mins)  # Number of periods to forecast
+        # forecast_90mins, conf_int_90mins = model_90mins.predict(n_periods=n_periods, return_conf_int=True)
+        # # forecast_index = pd.date_range(start=train['glucose mmol/l'].index[-1] + pd.Timedelta(days=1), periods=n_periods)
 
-    '''30 minute model'''
-    model_30mins = pm.auto_arima(train_30mins['Value'], 
-                      start_p=1, start_q=1,      # initial guesses for p and q
-                      max_p=10, max_q=10,          # maximum p and q values to try
-                      seasonal=True,            # no seasonality for CGM data
-                      stepwise=True,             # uses a stepwise approach to find the best model
-                      suppress_warnings=True,    # suppress warnings
-                      trace=False)                # output the results of the search process
+        '''30 minute model'''
+        model_30mins = pm.auto_arima(train_30mins['Value'], 
+                        start_p=1, start_q=1,      # initial guesses for p and q
+                        max_p=10, max_q=10,          # maximum p and q values to try
+                        seasonal=True,            # no seasonality for CGM data
+                        stepwise=True,             # uses a stepwise approach to find the best model
+                        suppress_warnings=True,    # suppress warnings
+                        trace=False)                # output the results of the search process
 
-    actuals_30mins = test_30mins['Value']
-    n_periods = len(actuals_30mins)  # Number of periods to forecast
-    forecast_30mins, conf_int_30mins = model_30mins.predict(n_periods=n_periods, return_conf_int=True)
-    # forecast_index = pd.date_range(start=train['glucose mmol/l'].index[-1] + pd.Timedelta(days=1), periods=n_periods)
+        actuals_30mins = test_30mins['Value']
+        n_periods = len(actuals_30mins)  # Number of periods to forecast
+        forecast_30mins, conf_int_30mins = model_30mins.predict(n_periods=n_periods, return_conf_int=True)
+        # forecast_index = pd.date_range(start=train['glucose mmol/l'].index[-1] + pd.Timedelta(days=1), periods=n_periods)
 
-    ''' Metrics: '''
-    print(f"\nForecasts for patient {patient_id} \n")
-    print(f'AIC: {model_90mins.aic()}')
-    print(f'Sigma^2: {np.var(model_90mins.resid())}')
-    print(f'Log likelihood: {model_90mins.arima_res_.llf}')
-    print(f"Patient {patient_id} MAE: {mean_absolute_error(actuals_90mins,forecast_90mins)}")
-    print(f"Patient {patient_id} MAPE: {mean_absolute_percentage_error(actuals_90mins,forecast_90mins)}")
-    mape_30mins = float(mean_absolute_percentage_error(actuals_30mins,forecast_30mins))
-    mape_90mins = float(mean_absolute_percentage_error(actuals_90mins,forecast_90mins))
-    print(f"Patinet {patient_id} MAPE 30mins/90mins: {mape_30mins/mape_90mins}")
-    print(f"Patient {patient_id} MSE: {mean_squared_error(actuals_90mins,forecast_90mins)}")
+        ''' Metrics: '''
+        MAE_i += mean_absolute_error([actuals_30mins.iloc[5]],[forecast_30mins.iloc[5]])
+        MAPE_i += mean_absolute_percentage_error([actuals_30mins.iloc[5]],[forecast_30mins.iloc[5]])
 
-    ''' Plotting: '''
-    plt.figure(figsize=(10,6))
-    plt.plot(test_90mins.index, forecast_90mins, label='Forecast', color='red')
-    plt.plot(test_90mins.index, actuals_90mins, label='Actual', color = 'blue')
-    plt.fill_between(test_90mins.index, conf_int_90mins[:, 0], conf_int_90mins[:, 1], color='pink', alpha=0.3)
-    plt.ylim(int(actuals_90mins.min() - 1), int(actuals_90mins.max() + 1))
-    plt.legend()
-    plt.savefig(f'./data/normal/predictions/auto_arima/p{patient_id}.png')
+    MAE_t += MAE_i/(len_data - cut_off - 6)
+    pt_num += 1
+    print(f'Progress: {pt_num}/{len(grouped_df)}')
 
-    ''' Saving Results: '''
-    results ={'Actuals' : actuals_90mins.reset_index(drop=True), 'Predictions' : forecast_90mins.reset_index(drop=True),
-            'Residuals' : actuals_90mins.reset_index(drop=True) - forecast_90mins.reset_index(drop=True) }
-    db_results = pd.DataFrame(results)
-    db_results.to_csv(f'./data/normal/predictions/auto_arima/p{patient_id}.csv')
-
+print(f"MAE: {MAE_t/len(grouped_df)}")
+print(f"MAPE: {MAPE_t/len(grouped_df)}")
 
 """Day/Night analysis """
 # db = pd.read_csv('./data/db_window_random_10days.csv')
